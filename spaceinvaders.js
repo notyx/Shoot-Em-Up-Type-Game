c63eb9e3b82a81b4e4425dbce4219435f4930a4b
notyx <119574611+notyx@users.noreply.github.com>
2023-05-15 09:43:17 +0300

    Update spaceinvaders.js

--- spaceinvaders.js
+++ spaceinvaders.js
@@ -1,3 +1,5 @@
-const canvas = document.getElementById("gameCanvas");
+window.addEventListener('load' , function(){
+const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
+});
 
