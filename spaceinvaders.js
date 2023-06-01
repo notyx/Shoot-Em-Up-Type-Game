c75f004b73c1339f53fa6b43d9e75ecaa30164e4
notyx <119574611+notyx@users.noreply.github.com>
2023-06-01 06:13:10 +0200

    Update spaceinvaders.js

--- spaceinvaders.js
+++ spaceinvaders.js
@@ -1,42 +1,48 @@
-window.addEventListener('load' , function(){
-    const canvas = document.getElementById("canvas1");
-    const ctx = canvas.getContext("2d");
-    canvas.width = 500;
-    canvas.height =500;
-  
-    class InputHandler {
-      
-    }
-    class Projectile {
-      
-    }
-    class Player {
-      
-    }
-    class Enemy {
-      
-    }
-    class Background {
-        
-    }
-    class UI {
-        
-    }
-    class Game {
-    
-    }
- 
+let canvas = document.getElementById('myCanvas');
+  /** @type {CanvasRenderingContext2D}  */
+let ctx = canvas.getContext("2d");
+
+
+(function () {
+  var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+  window.requestAnimationFrame = requestAnimationFrame;
+  var cancvelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame;
+})();
+var animationID;
+
+// image sprites
+let spriteBase64 = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAAEACAYAAAADRnAGAAACGUlEQVR42u3aSQ7CMBAEQIsn8P+/hiviAAK8zFIt5QbELiTHmfEYE3L9mZE9AAAAqAVwBQ8AAAD6THY5CgAAAKbfbPX3AQAAYBEEAADAuZrC6UUyfMEEAIBiAN8OePXnAQAAsLcmmKFPAQAAgHMbm+gbr3Sdo/LtcAAAANR6GywPAgBAM4D2JXAAABoBzBjA7AmlOx8AAEAzAOcDAADovTc4vQim6wUCABAYQG8QAADd4dPd2fRVYQAAANQG0B4HAABAawDnAwAA6AXgfAAAALpA2uMAAABwPgAAgPoAM9Ci/R4AAAD2dmqcEQIAIC/AiQGuAAYAAECcRS/a/cJXkUf2AAAAoBaA3iAAALrD+gIAAADY9baX/nwAAADNADwFAADo9YK0e5FMX/UFACA5QPSNEAAAAHKtCekmDAAAAADvBljtfgAAAGgMMGOrunvCy2uCAAAACFU6BwAAwF6AGQPa/XsAAADYB+B8AAAAtU+ItD4OAwAAAFVhAACaA0T7B44/BQAAANALwGMQAAAAADYO8If2+P31AgAAQN0SWbhFDwCAZlXgaO1xAAAA1FngnA8AACAeQPSNEAAAAM4CnC64AAAA4GzN4N9NSfgKEAAAAACszO26X8/X6BYAAAD0Anid8KcLAAAAAAAAAJBnwNEvAAAA9Jns1ygAAAAAAAAAAAAAAAAAAABAQ4COCENERERERERERBrnAa1sJuUVr3rsAAAAAElFTkSuQmCC";
+const tank = new Image();
+tank.src = spriteBase64;
+const invader = new Image();
+invader.src = spriteBase64;
+var startScreenTimeout;
+//  tank et sprite
+
+var frameCount=0;
+var armyPrevFrameCount=0;
+var framesInOneSec = 1000/16;
+var spritUnitHeight = 35;
+var spriteUnitWidth = 64;
+var scoreBarHeight = 50;
+var tank__bottomOffset = (spritUnitHeight/2) + scoreBarHeight;
+var tankX=canvas.width/2;
+var tankdX = 4;
+var tankY=canvas.height-(tank__bottomOffset);
+var tankWidth= spriteUnitWidth/2;
+var tankHeight =spritUnitHeight/2
+var keys =[];
+
+// score et vies
+var score = 0;
+var allowedLives = 3;
+var lives = allowedLives;
+var hasLifeDecreased = false;
+var gameRunning = false;
 
 
 
 
 
 
-
-
-
-
-
-
-});
 
