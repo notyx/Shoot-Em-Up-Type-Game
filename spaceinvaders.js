005c41a3358f5652747759149100afeddecfa4a7
notyx <119574611+notyx@users.noreply.github.com>
2023-06-04 21:34:55 +0200

    Update spaceinvaders.js

--- spaceinvaders.js
+++ spaceinvaders.js
@@ -204,9 +204,7 @@ function invadersBulletHandler(){
 }
 
 function generateInvaderRandomBullet(){
-    // let randomInvaderR = genRandomNumber(armyRows); 
-    // let randomInvaderC = genRandomNumber(armyColumns); 
-    // let rInvader = armyArray[randomInvaderR][randomInvaderC];
+    // Tire des Invaders quand le jouer est en vie
     let aliveArmy = [];
     for (let i = 0; i < armyRows; i++) {    
       for(let j = 0; j < armyColumns; j++){
@@ -263,6 +261,396 @@ function moveInvaderBullets(){
 
   }
 }
+function helperHandler(){
+  if(aliveInvaders == armyColumns* armyRows){
+    drawBottomMessage("press SPACE to fire bullet", 125);
+  }  else
+  if(hasLifeDecreased){
+    drawBottomMessage(`HIT. Lives Left: ${lives}`, 150);
+    setTimeout(() => {
+    hasLifeDecreased=false;
+    drawBottomMessage(``, 150);      
+    }, 2000);
+  }
+
+}
+
+// draw fonctions
+function drawInvader(x,y,sHeight){
+  ctx.beginPath();
+  ctx.drawImage(// Image
+    invader,
+    // ---- Selection ----
+    0, // sx
+    sHeight, // sy
+    spriteUnitWidth, // sWidth
+    spritUnitHeight, // sHeight
+    // ---- Drawing ----
+    x, // dx
+    y, // dy
+    invaderWidth, // dWidth
+    invaderHeight // dHeight 
+    );    
+    ctx.closePath();
+}
+
+
+function drawTank(x,y){
+  ctx.beginPath();
+  ctx.drawImage(// Image
+    tank,
+    // ---- Selection ----
+    0, // sx
+    tank.height-50, // sy
+    tank.width, // sWidth
+    spritUnitHeight, // sHeight
+    // ---- Drawing ----
+    x, // dx
+    y, // dy
+    tankWidth, //42, // dWidth
+    tankHeight //24 // dHeight 
+    );
+ctx.closePath();
+}
+
+// mouvement de l'armée des invaders
+function moveArmy(){
+  if(frameCount-armyPrevFrameCount>armySpeed){
+    armyPrevFrameCount=frameCount;
+    invaderSpriteHeight=spritUnitHeight-invaderSpriteHeight;
+    spriteSelector= 1 - spriteSelector;
+  }
+  else{
+    return false;
+  }
+  let dx;
+  let dy=0;
+  if (armyDirection == 'right') {
+    if(canvas.width - (armyX + (invaderWidth+invaderLeftOffset)*(armyColumns-1)) > invaderWidth){
+      dx=1;
+    }else{
+      armyDirection='left';
+      dx=-1;
+      dy=armyDy;
+    }
+            
+  } else
+  if (armyDirection == 'left') {
+      if (armyX-armyDx>0) {
+      dx=-1;        
+      }else{
+        armyDirection='right';
+        dx=1;
+        dy=armyDy;
+      }
+      
+  }
+
+  armyX+=armyDx*(dx);
+  updateArmy(dx*(armyDx),dy)
+}
+
+// formation de l'armée 
+function constructArmy(aX,aY){
+  for (let i = 0; i < armyRows; i++) {
+    armyArray[i]=[];
+    for(let j = 0; j < armyColumns; j++){
+      armyArray[i][j]={
+        x: aX + j*(invaderWidth + invaderLeftOffset),
+        y:aY + i*(invaderHeight + invaderTopOffset),
+        status:"alive"
+      };
+    }
+  }
+}
+
+function updateArmy(adx,ady){    
+  for (let i = 0; i < armyRows; i++) {    
+    for(let j = 0; j < armyColumns; j++){
+      let soldier = armyArray[i][j];
+      soldier.x = soldier.x+(adx);
+      soldier.y = soldier.y + ady;
+      
+    }
+  }
+}
+
+function drawArmyOfInvaders(){
+  for (let i = 0; i < armyRows; i++) {
+    for(let j = 0; j < armyColumns; j++){
+        let soldier = armyArray[i][j];
+        if (soldier.status=='alive') {
+          // dessins des invaders(soldier.x,soldier.y,invaderSpriteHeight);
+          drawInvader(soldier.x,soldier.y,invaderSpriteHeightsArray[i][spriteSelector]);
+            // vérifier si le jeu est terminé par collision
+            if(soldier.y > tankY){
+              gameRunning=false;
+            }
+        }
+      
+    }
+  }  
+}
+
+
+function drawBullet(bx,by){
+  ctx.beginPath();
+  ctx.beginPath();       // Start a new path
+  ctx.moveTo(bx, by);    // Bouge le stylo à (30, 50)
+  ctx.lineTo(bx, by-bullet__height);  // Dessine une ligne à (150, 100)
+  ctx.lineWidth = bullet__width+2;
+  ctx.strokeStyle = "green";
+  ctx.stroke();
+}
+
+function fireTankBullet(){
+    tankBullet__x = tankX + tankWidth/2 ;
+    tankBullet__y = canvas.height - tank__bottomOffset;
+    drawBullet(tankBullet__x,tankBullet__y);
+    moveTankBullet();
+    shouldMoveTankBullet = true;
+}
+
+function moveTankBullet(){
+    if(tankBullet__y < 0){
+      shouldMoveTankBullet= false;      
+    }
+    // vérifier si un envahisseur est touché par la balle
+    for (let i = 0; i < armyRows; i++) {
+      for(let j = 0; j < armyColumns; j++){
+          let soldier = armyArray[i][j];
+          if(
+            tankBullet__x > soldier.x &&
+            tankBullet__x < soldier.x + invaderWidth &&
+            tankBullet__y > soldier.y &&
+            tankBullet__y < soldier.y + invaderHeight &&
+            soldier.status == 'alive'
+          )
+          {
+            soldier.status='dead';
+            shouldMoveTankBullet=false;
+            aliveInvaders--;
+            score++;            
+            explosionColor = "white";
+            particlesPerExplosion   = 50;
+            particlesMaxSize        = 3;
+            triggerExplosion(soldier.x+invaderWidth/2,soldier.y+invaderHeight/2);
+            // accélération de l'armée
+            if((aliveInvaders)%armyColumns==0 && armySpeed > 10){
+              armySpeed-=armySpeed__decrement;
+              console.log("speed increase: " + armySpeed);
+            }
+          }
+                  
+      }
+    }  
+    // vérifier si un invader est touché par la balle   
+
+    tankBullet__y -= tankBullet__dy;
+}
+// Score
+function drawScoreSeprateLine(){
+  ctx.beginPath();
+  ctx.beginPath();       // commence une nouvelle path
+  ctx.moveTo(0, canvas.height- (scoreBarHeight-15));    // Bouge le stylo à  (30, 50)
+  ctx.lineTo(canvas.width, canvas.height - (scoreBarHeight-15));  // Dessine une ligne à (150, 100)
+  ctx.lineWidth = 2;
+  ctx.shadowBlur = 5;
+  ctx.shadowOffsetY=1;
+  ctx.shadowOffsetY=-1;
+  ctx.shadowColor="green";
+  ctx.strokeStyle = "green";
+  ctx.stroke();
+  ctx.shadowBlur = 0;
+  ctx.shadowOffsetY=0;
+  ctx.shadowOffsetY=0;  
+}
+
+
+
+function drawBottomMessage(message,sx){
+  ctx.beginPath();
+  ctx.font = "20px Play";
+  ctx.fillStyle="white";
+  ctx.fillText(message, sx, canvas.height-10);  
+  ctx.closePath();
+}
+function drawScore(){
+  drawBottomMessage("Score: "+score,canvas.width - 90)
+}
+function drawLives(){
+  drawBottomMessage("Lives: "+lives,10)
+}
+
+function drawBottomHelper(){
+  drawBottomMessage("Press enter to play",150)
+}
+
+
+function drawInvaderBullet(ix, iy){
+  ctx.beginPath();
+  ctx.beginPath();       // commence une nouvelle path
+  ctx.moveTo(ix, iy);    // Bouge le stylo à (30, 50)
+  ctx.lineTo(ix, iy+bullet__height);  // Dessine une ligne à (150, 100)
+  ctx.lineWidth = bullet__width;
+  ctx.strokeStyle = "#FFF";
+  ctx.stroke();
+}
+
+function drawGameOver(message){
+  drawBlinker(function(){ drawScreen__line1("Game Over ") },function(){ drawScreen__line2(message) });
+}
+
+// dessine un clignotant
+function drawBlinker(func1, func2){
+  let counter=0;
+  startScreenTimeout = setInterval(() => {
+    ctx.clearRect(0,0,canvas.width,canvas.height-50);
+    func1();
+    if(counter%3==0)func2();
+    counter++;
+  }, 400);
+}
+  
+  function drawStartScreen(){
+    drawBlinker(function(){ drawScreen__line1("Space Invaders") },function(){ drawScreen__line2("press enter to play") });
+  }
+
+
+
+function drawScreen__line1(message){
+    ctx.save();
+    ctx.beginPath();
+    ctx.font = "60px Play";
+    ctx.fillStyle="white";
+    ctx.textAlign = "center";
+    ctx.fillText(message, canvas.width/2,canvas.height/2);  
+    ctx.closePath();
+    ctx.restore();
+}
+function drawScreen__line2(message){
+    ctx.save();
+    ctx.beginPath();
+    ctx.fillStyle="green";
+    ctx.textAlign = "center";
+    ctx.font = "40px Play";
+    ctx.fillText(message, canvas.width/2,canvas.height/2+60);  
+    ctx.closePath();
+    ctx.restore();
+}
+
+// fonctions des explosions
+
+// Dessin des explosions
+function drawExplosion() {
+
+  if (explosions.length === 0) {
+    return;
+  }
+
+  for (let i = 0; i < explosions.length; i++) {
+
+    const explosion = explosions[i];
+    const particles = explosion.particles;
+
+    if (particles.length === 0) {
+      explosions.splice(i, 1);
+      return;
+    }
+
+    const particlesAfterRemoval = particles.slice();
+    for (let ii = 0; ii < particles.length; ii++) {
+
+      const particle = particles[ii];
+
+      // Vérifier la taille des particules
+      // Si 0, supprimer
+      if (particle.size <= 0) {
+        particlesAfterRemoval.splice(ii, 1);
+        continue;
+      }
+
+      ctx.beginPath();
+      ctx.arc(particle.x, particle.y, particle.size, Math.PI * 2, 0, false);
+      ctx.closePath();
+    
+      ctx.fillStyle = explosionColor;
+      ctx.fill();
+
+      // mise à jour
+      particle.x += particle.xv;
+      particle.y += particle.yv;
+      particle.size -= .1;
+    }
+
+    explosion.particles = particlesAfterRemoval;
+
+  }
+
+}
+
+
+// CLick
+function triggerExplosion(triggerX,triggerY) {
+
+  let xPos, yPos;
+
+  xPos = triggerX;
+    yPos = triggerY;
+  explosions.push(
+    new explosion(xPos, yPos)
+  );
+
+}
+
+// explosions
+function explosion(x, y) {
+
+  this.particles = [];
+
+  for (let i = 0; i < particlesPerExplosion; i++) {
+    this.particles.push(
+      new particle(x, y)
+    );
+  }
+
+}
+
+// Particules
+function particle(x, y) {
+  this.x    = x;
+  this.y    = y;
+  this.xv   = randInt(particlesMinSpeed, particlesMaxSpeed, false);
+  this.yv   = randInt(particlesMinSpeed, particlesMaxSpeed, false);
+  this.size = randInt(particlesMinSize, particlesMaxSize, true);
+  this.r    = randInt(113, 222);
+  this.g    = '00';
+  this.b    = randInt(105, 255);
+}
+
+// Returns an random integer, positive or negative
+// between the given value
+function randInt(min, max, positive) {
+
+  let num;
+  if (positive === false) {
+    num = Math.floor(Math.random() * max) - min;
+    num *= Math.floor(Math.random() * 2) === 1 ? 1 : -1;
+  } else {
+    num = Math.floor(Math.random() * max) + min;
+  }
+
+  return num;
+
+}
+
+// // On-click
+// document.addEventListener('click',(e)=>{
+//   triggerExplosion(e);
+// });
+
+
+// ################################################################### Explosion functions
 
     
   
