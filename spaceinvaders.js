322c965db903bfbe5a190ebcaeaaf638e3e39e4e
notyx <119574611+notyx@users.noreply.github.com>
2023-06-05 06:22:02 +0200

    Update spaceinvaders.js

--- spaceinvaders.js
+++ spaceinvaders.js
@@ -13,7 +13,7 @@ let ctx = canvas.getContext("2d");
 })();
 var animationID;
 
-//  charge sprites
+//  charge les sprites
 let spriteBase64 = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAAEACAYAAAADRnAGAAACGUlEQVR42u3aSQ7CMBAEQIsn8P+/hiviAAK8zFIt5QbELiTHmfEYE3L9mZE9AAAAqAVwBQ8AAAD6THY5CgAAAKbfbPX3AQAAYBEEAADAuZrC6UUyfMEEAIBiAN8OePXnAQAAsLcmmKFPAQAAgHMbm+gbr3Sdo/LtcAAAANR6GywPAgBAM4D2JXAAABoBzBjA7AmlOx8AAEAzAOcDAADovTc4vQim6wUCABAYQG8QAADd4dPd2fRVYQAAANQG0B4HAABAawDnAwAA6AXgfAAAALpA2uMAAABwPgAAgPoAM9Ci/R4AAAD2dmqcEQIAIC/AiQGuAAYAAECcRS/a/cJXkUf2AAAAoBaA3iAAALrD+gIAAADY9baX/nwAAADNADwFAADo9YK0e5FMX/UFACA5QPSNEAAAAHKtCekmDAAAAADvBljtfgAAAGgMMGOrunvCy2uCAAAACFU6BwAAwF6AGQPa/XsAAADYB+B8AAAAtU+ItD4OAwAAAFVhAACaA0T7B44/BQAAANALwGMQAAAAADYO8If2+P31AgAAQN0SWbhFDwCAZlXgaO1xAAAA1FngnA8AACAeQPSNEAAAAM4CnC64AAAA4GzN4N9NSfgKEAAAAACszO26X8/X6BYAAAD0Anid8KcLAAAAAAAAAJBnwNEvAAAA9Jns1ygAAAAAAAAAAAAAAAAAAABAQ4COCENERERERERERBrnAa1sJuUVr3rsAAAAAElFTkSuQmCC";
 const tank = new Image();
 tank.src = spriteBase64;
@@ -173,7 +173,7 @@ function gameLoop(){
 
 
 
-// Events
+// Events de touches
 window.addEventListener("keydown", ()=>keys[event.keyCode] = true);
 window.addEventListener("keyup", ()=>keys[event.keyCode] = false);
 window.addEventListener("keypress", keypressedHandler);
@@ -266,7 +266,7 @@ function moveInvaderBullets(){
 
   }
 }
-
+// affiche ui de la partie
 function helperHandler(){
   if(aliveInvaders == armyColumns* armyRows){
     drawBottomMessage("press SPACE to fire bullet", 125);
